986f5fc10e940f8c8f34ed400fb6eed0d7a61ec6
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-01-27 13:50:17 +0530

    feat: add loading state to Forgot Password button

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { forgotPassword } from "../../features/Actions/ForgotPassword/ForgotPasswordAction";
 
 const ForgotPassword = () => {
@@ -10,6 +10,8 @@ const ForgotPassword = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  const { isLoading } = useSelector((state) => state.password);
   const submitForm = (data) => {
     dispatch(forgotPassword(data));
   };
@@ -52,12 +54,21 @@ const ForgotPassword = () => {
                   {...register("email")}
                 />
               </div>
-              <button
-                type="submit"
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Sign in
-              </button>
+              {isLoading ? (
+                <button
+                  type="submit"
+                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Loading...
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
+              )}
             </form>
           </div>
         </div>
